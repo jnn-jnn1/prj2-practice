d08eb9da2736db3b948a174615f24e1ef5f0c1b0
jnn-jnn1 <anj72005@gmail.com>
2024-05-23 20:10:14 +0900

    initial commit

---
+++ src/main/java/com/example/prj2practice/Prj2PracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.prj2practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prj2PracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prj2PracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/prj2practice/Prj2PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prj2practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prj2PracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
